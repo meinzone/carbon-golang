68d8280c1fc07cde82afd05b580960bb7ff86c0b
Jeffrey Forman <jeff@jeffreyforman.net>
2016-03-13 16:21:50 -0400

    create a single-metric send function, as well as clean up verbosity

--- carbon.go
+++ carbon.go
@@ -13,6 +13,7 @@ type Carbon struct {
 	conn     net.Conn
 	Noop     bool
 	Protocol string
+	Verbose  bool
 }
 
 // time in Seconds of how long each Carbon transaction is allowed to take
@@ -38,9 +39,9 @@ func (carbon *Carbon) Connect() error {
 		if carbon.conn != nil {
 			carbon.conn.Close()
 		}
-
 		if !carbon.IsNoop() {
 			address := fmt.Sprintf("%s:%d", carbon.Host, carbon.Port)
+			fmt.Printf("Carbon Reciever: %v.\n", address)
 			conn, err := net.DialTimeout(carbon.Protocol, address, carbon.Timeout)
 			if err != nil {
 				return err
@@ -51,25 +52,43 @@ func (carbon *Carbon) Connect() error {
 	return nil
 }
 
+// Given a carbon Metric, send it to Carbon.
+func (carbon *Carbon) SendMetric(metric Metric) error {
+	_, err := fmt.Fprintf(carbon.conn, metric.String()+"\n")
+	if carbon.Verbose {
+		fmt.Printf("in SendMetrics, metric.String: %s\n", metric.String())
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Given a list of metrics, send them each invidually.
 func (carbon *Carbon) SendMetrics(metrics []Metric) error {
 	if carbon.IsDefined() {
 		if carbon.IsNoop() {
 			return nil
 		}
+		start := time.Now()
 		for _, metric := range metrics {
-			//			fmt.Printf("in SendMetrics, metric.String: %s\n", metric.String())
-			_, err := fmt.Fprintf(carbon.conn, metric.String()+"\n")
+			err := carbon.SendMetric(metric)
 			if err != nil {
 				return err
 			}
+		}
+		elapsed := time.Since(start)
+		if carbon.Verbose {
+			fmt.Printf("Elapsed time to post to Carbon %v.\n", elapsed)
 		}
 	}
 	return nil
 }
 
 // Create new Carbon object as well as connect to Carbon instance.
-func NewCarbon(host string, port int, noop bool) (*Carbon, error) {
-	carbon := &Carbon{Host: host, Port: port, Timeout: time.Duration(1 * time.Minute), Protocol: "tcp", Noop: noop}
+func NewCarbon(host string, port int, noop bool, verbose bool) (*Carbon, error) {
+	carbon := &Carbon{Host: host, Port: port, Timeout: time.Duration(1 * time.Minute), Protocol: "tcp", Noop: noop,
+		Verbose: verbose}
 	err := carbon.Connect()
 	if err != nil {
 		return nil, err
